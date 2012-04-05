6d45d2ce4ad2e117277aeda3462237080e9dc21c
Frederick Schoeneman <fred.schoeneman@gmail.com>
2012-04-04 19:18:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftacular::Application.config.secret_token = '7f43bd87a0ad37cd18f16a695cd57bec2bafdf58de309a89ce0c448899e0232478baa00657ca3757f782e706001b6217f3db74b7a96e95c152b52a9d89bbac93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Giftacular::Application.config.session_store :cookie_store, key: '_giftacular_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Giftacular::Application.config.session_store :active_record_store
